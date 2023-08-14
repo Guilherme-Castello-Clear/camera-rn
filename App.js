4e7febf0edc434c6f96a35f31743f94fb626a153
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-08-13 23:11:05 -0300

    Feat: Initialized project (installed camera)

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text, StatusBar } from "react-native";
+
+export default function App(){
+  return(
+    <View>
+      <StatusBar hidden={true}/>
+      <Text>A</Text>
+    </View>
+  )
+}
